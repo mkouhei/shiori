4d8b2bc0ab11d5bc7063431171e57370de4bba7d
Kouhei Maeda <mkouhei@palmtb.net>
2014-02-28 06:26:45 +0900

    Add unittest of routing.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- shiori/api/tests.py
+++ shiori/api/tests.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from api.views import CategoryViewSet
 
-# Create your tests here.
+
+class APITest(TestCase):
+
+    def test_api_categories_url_resolve(self):
+        response = self.client.get('/v1/categories')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_tags_url_resolve(self):
+        response = self.client.get('/v1/tags')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_bookmarks_url_resolve(self):
+        response = self.client.get('/v1/bookmarks')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_bookmark_tags_url_resolve(self):
+        response = self.client.get('/v1/bookmark_tags')
+        self.assertEqual(response.status_code, 200)
--- shiori/bookmark/tests.py
+++ shiori/bookmark/tests.py
@@ -1,3 +1,42 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+import bookmark.views
 
-# Create your tests here.
+
+class BookmarkTest(TestCase):
+    def test_shiori_url_resolve(self):
+        found = resolve('/shiori/')
+        self.assertEqual(found.func, bookmark.views.index)
+
+    def test_profile_url_resolve(self):
+        found = resolve('/shiori/profile/')
+        self.assertEqual(found.func, bookmark.views.profile)
+
+    def test_add_url_resolve(self):
+        found = resolve('/shiori/add/')
+        self.assertEqual(found.func, bookmark.views.add)
+
+    def test_categories_url_resolve(self):
+        found = resolve('/shiori/categories')
+        self.assertEqual(found.func, bookmark.views.categories)
+
+    def test_category_url_resolve(self):
+        found = resolve('/shiori/categories/dummy_id')
+        self.assertEqual(found.func, bookmark.views.category)
+
+    def test_tags_url_resolve(self):
+        found = resolve('/shiori/tags')
+        self.assertEqual(found.func, bookmark.views.tags)
+
+    def test_tag_url_resolve(self):
+        found = resolve('/shiori/tags/dummy_id')
+        self.assertEqual(found.func, bookmark.views.tag)
+
+    def test_feeds_url_resolve(self):
+        response = self.client.get('/shiori/feeds')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bookmark_url_resolve(self):
+        found = resolve('/shiori/b/dummy_id')
+        self.assertEqual(found.func, bookmark.views.bookmark)
