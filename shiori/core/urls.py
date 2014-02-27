16e30145d260a85a1b72e067e2ef52ca69279308
Kouhei Maeda <mkouhei@palmtb.net>
2014-02-27 13:01:47 +0900

    Redirect root to /shiori/.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- shiori/core/urls.py
+++ shiori/core/urls.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
                        (r'', include('django_openid_auth.urls')),
+                       (r'^$', RedirectView.as_view(url='/shiori/')),
                        (r'^openid/', include('django_openid_auth.urls')),
                        (r'^logout/$', 'django.contrib.auth.views.logout',
                         {'template_name': 'bookmark/logout.html'}),
