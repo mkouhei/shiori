eb9adc346933f27acdff4da6a51b138bcdeee1a3
Kouhei Maeda <mkouhei@palmtb.net>
2014-04-03 17:12:24 +0900

    Added list display at Django admin.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- shiori/bookmark/admin.py
+++ shiori/bookmark/admin.py
@@ -8,9 +8,29 @@ from shiori.bookmark.models import (Bookmark,
                                     CrawlingHistory)
 
 
-admin.site.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'registered_datetime',
+                    'owner', 'is_hide')
+
+
+class BookmarkTagAdmin(admin.ModelAdmin):
+    list_display = ('bookmark', 'tag')
+
+
+class FeedSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'default_category')
+
+
+class CrawlingHistoryAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'update_datetime', 'result')
+
+    def get_name(self, obj):
+        return obj.feed.name
+
+
+admin.site.register(Bookmark, BookmarkAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(BookmarkTag)
-admin.site.register(FeedSubscription)
-admin.site.register(CrawlingHistory)
+admin.site.register(BookmarkTag, BookmarkTagAdmin)
+admin.site.register(FeedSubscription, FeedSubscriptionAdmin)
+admin.site.register(CrawlingHistory, CrawlingHistoryAdmin)
